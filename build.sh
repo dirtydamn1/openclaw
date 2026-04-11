aa50239d96d337d30f4b8154267b8a74249ed00b
liuguanliang <liuguanliang@tasyee.com>
2026-04-11 23:12:12 +0800

    build.sh

---
+++ build.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# 从源码安装
+pnpm install && pnpm ui:build && pnpm build
+pnpm link --global
+# 运行向导，并安装系统自启服务
+# openclaw onboard --install-daemon
+openclaw onboard
+
+# openclaw gateway stop
+# openclaw gateway start
+# openclaw gateway status
