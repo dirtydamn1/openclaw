124766bbda4582aaa158e11b314f203f0fa4d5a9
liuguanliang <liuguanliang@tasyee.com>
2026-04-18 23:08:38 +0800

    v2026.4.15

--- update-docker-compose.sh
+++ update-docker-compose.sh
@@ -7,7 +7,7 @@ export OPENCLAW_HOME_VOLUME="openclaw_home"
 # 指定浏览器安装路径并确保其在持久化范围内
 export PLAYWRIGHT_BROWSERS_PATH=/home/node/.cache/ms-playwright
 # 安装额外的apt包，jq后面为browser需要的依赖，fonts-wqy-zenhei为中文字体(截图时中文不会乱码)
-export OPENCLAW_DOCKER_APT_PACKAGES="curl less vim lsof wget net-tools git jq fonts-wqy-zenhei libnss3 libnspr4 libatk1.0-0 libatk-bridge2.0-0 libcups2 libdrm2 libxcb1 libxkbcommon0 libxcomposite1 libxdamage1 libxrandr2 libgbm1 libasound2 libxfixes3 "
+export OPENCLAW_DOCKER_APT_PACKAGES="curl less vim lsof wget net-tools netcat-openbsd telnet git jq fonts-wqy-zenhei libnss3 libnspr4 libatk1.0-0 libatk-bridge2.0-0 libcups2 libdrm2 libxcb1 libxkbcommon0 libxcomposite1 libxdamage1 libxrandr2 libgbm1 libasound2 libxfixes3 "
 # 工作目录
 export OPENCLAW_CONFIG_DIR=/home/ops/.openclaw
 export OPENCLAW_WORKSPACE_DIR=/home/ops/.openclaw/workspace
@@ -23,7 +23,7 @@ export OPENCLAW_INSTALL_DOCKER_CLI=1
 # export OPENCLAW_SANDBOX=1
 # 额外挂载路径
 export OPENCLAW_EXTRA_MOUNTS=
-export OPENCLAW_IMAGE=v2026.4.14
+export OPENCLAW_IMAGE=v2026.4.15
 ######feature-dirtydamn end######
 
 docker compose -f docker-compose.yml -f docker-compose.extra.yml up -d openclaw-gateway
